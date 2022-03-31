e74241c3b0fa1c7a83e4f34bf4d0e6708d2fc6ef
martineleirvik <leirvikmartine@gmail.com>
2022-03-31 15:07:23 +0200

    dark-light mode

--- js/dark-light-theme.js
+++ js/dark-light-theme.js
@@ -1,7 +1,30 @@
 const icon = document.querySelector("#icon")
 
+if(localStorage.getItem("theme") === null) {
+    localStorage.setItem("theme", "light");
+}
+
+let localData = localStorage.getItem("theme");
+
+if (localData === "light") {
+    icon.src = "images/moon.png";
+    document.body.classList.remove("dark-mode")
+}
+else if(localData === "dark") {
+    icon.src = "images/sun.png";
+    document.body.classList.add("dark-mode")
+}
+
 icon.addEventListener("click", changeMode);
 
 function changeMode() {
-    document.body.class
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")) {
+        icon.src = "images/sun.png";
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        icon.src= "images/moon.png";
+        localStorage.setItem("theme", "light");
+    }
 }
